d5f5d232b9a46208abd7e8ef5eb2a3f2c2dff379
jfzhang95 <jfzhang@whu.edu.cn>
2017-08-28 10:10:40 +0800

    update

--- news_spider/pipelines.py
+++ news_spider/pipelines.py
@@ -1,11 +1,33 @@
-# -*- coding: utf-8 -*-
+# -*- coding: gbk -*-
 
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import codecs
+import json
+import os
+
 
 class NewsSpiderPipeline(object):
+    def __init__(self):
+        self.current_dir = os.getcwd()
+
     def process_item(self, item, spider):
+        dir_path = self.current_dir + '/docs' + item['source'] + '/' + item['date']
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        news_file_path = dir_path + '/' + item['newsId'] + '.json'
+        if os.path.exists(news_file_path) and os.path.isfile(news_file_path):
+            print('---------------------------------------')
+            print(item['newsId'] + '.json exists, not overriden')
+            print('---------------------------------------')
+            return item
+
+
+        news_file = codecs.open(news_file_path, 'w', 'gbk')
+        line = json.dumps(dict(item))
+        news_file.write(line)
+        news_file.close()
         return item
--- news_spider/settings.py
+++ news_spider/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'news_spider.pipelines.NewsSpiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'news_spider.pipelines.NewsSpiderPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
